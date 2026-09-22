b7ecaf75f21b9c49ec9be76843dc5edaf8b84344
Jakub Dziektarz <73693198+MrZakurzacz@users.noreply.github.com>
2026-09-22 15:22:14 +0200

    Connect encoder output to network transport

--- FreeCamIP/App/FreeCamModel.swift
+++ FreeCamIP/App/FreeCamModel.swift
@@ -1,9 +1,12 @@
 import CoreMedia
 import Foundation
+import UIKit
 
 final class FreeCamModel: ObservableObject {
     let camera = CameraCaptureService()
+    let transport = TransportService()
 
+    @Published var pcAddress = ""
     @Published private(set) var encodedFrames: Int = 0
     @Published private(set) var lastAccessUnitBytes: Int = 0
     @Published private(set) var lastFrameWasKeyFrame: Bool = false
@@ -23,13 +26,32 @@ final class FreeCamModel: ObservableObject {
             )
         }
 
-        encoder.onAccessUnit = { [weak self] data, isKeyFrame, _ in
+        encoder.onAccessUnit = { [weak self] data, isKeyFrame, timestamp in
+            guard let self else { return }
+
+            self.transport.sendAccessUnit(
+                data,
+                isKeyFrame: isKeyFrame,
+                presentationTimeStamp: timestamp
+            )
+
             DispatchQueue.main.async {
-                self?.encodedFrames += 1
-                self?.lastAccessUnitBytes = data.count
-                self?.lastFrameWasKeyFrame = isKeyFrame
+                self.encodedFrames += 1
+                self.lastAccessUnitBytes = data.count
+                self.lastFrameWasKeyFrame = isKeyFrame
             }
         }
+    }
+
+    func connect() {
+        transport.connect(
+            host: pcAddress,
+            deviceName: UIDevice.current.name
+        )
+    }
+
+    func disconnect() {
+        transport.disconnect()
     }
 
     func start() {
